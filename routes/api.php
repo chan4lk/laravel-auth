20b5675b7964db7f77d821f78fe5965216ed8804
Chandima Ranaweera <chan4lk@gmail.com>
2021-03-13 19:20:06 +0530

    registered

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,21 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+    // ...
+
+    Route::post('/login', 'App\Http\Controllers\Auth\ApiAuthController@login')->name('login.api');
+    Route::post('/register','App\Http\Controllers\Auth\ApiAuthController@register')->name('register.api');
+});
+
+Route::middleware('auth:api')->group(function () {
+    // our routes to be protected will go in here
+    Route::post('/logout', 'App\Http\Controllers\Auth\ApiAuthController@logout')->name('logout.api');
+
+});
+
+
